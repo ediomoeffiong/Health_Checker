2c5cae7ee35412f397afe6c6b2181711f8f53e11
Covenant <ediomoemmaeffiong@gmail.com>
2024-06-27 13:57:04 +0100

    Structure updated, code is working but not fully functional, conditions still need to be added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 import java.util.Scanner;
 public class Main {
+    public static boolean isFound;
+    public static boolean isFemale, isMale;
+    public static boolean cough, vomiting, fatigue, headache, fever, chill, discomfort, nausea, pain, drowsiness;
+    public static boolean dehydration;
+    public static boolean lost_appetite;
     public static void main(String[] args) {
         System.out.println("-------------------Health Checker-------------------");
         System.out.print("Enter your name: ");
@@ -7,15 +12,96 @@ public class Main {
         String userName = scanName.nextLine();
         System.out.println("\n");
         System.out.println("Hii, " + userName + " I hope you're doing good today.");
-        System.out.println("Warning: Enter the correct details throughout this prompt!");
-        System.out.println("Type in the symptoms you're are having separated by a comma and followed by a space.");
-        System.out.println("Hit enter when you're done");
-        System.out.print("Symptoms: ");
+        System.out.println("\nWarning: Enter the correct details throughout this prompt!");
+        System.out.print("Input your gender (male or female): ");
+        Scanner scanGender = new Scanner(System.in);
+        String userGender = scanGender.nextLine();
+        if (userGender.equals("male") || userGender.equals("Male")) {
+            isMale = true;
+        } else {
+            isFemale = true;
+        }
+
+        System.out.println("\nType in the symptoms you're are having separated by a comma and followed by a space.");
+        System.out.println("\nHit enter when you're done");
+        System.out.print("\nInput symptoms: ");
         Scanner scanSymptom = new Scanner(System.in);
         String userSymptom = scanSymptom.nextLine();
         String[] userSymptoms = userSymptom.split(", ");
-        for (String symptom : userSymptoms) {
-            System.out.println(symptom);
+
+        //Turns every element in the array to small letters
+        for (int i = 0; i < userSymptoms.length; i++) {
+            userSymptoms[i] = userSymptoms[i].toLowerCase();
         }
+
+        System.out.println("\n");
+        symptoms(userSymptoms);
+    }
+
+    public static void symptoms(String[] symptom) {
+        for (String symptoms : symptom) {
+            if (symptoms.equals("cough")) {
+                cough = true;
+            }
+            if (symptoms.equals("fever")) {
+                fever = true;
+            }
+            if (symptoms.equals("headache")) {
+                headache = true;
+            }
+            if (symptoms.equals("vomiting")) {
+                vomiting = true;
+            }
+            if (symptoms.equals("fatigue")) {
+                fatigue = true;
+            }
+            if (symptoms.equals("chill")) {
+                chill = true;
+            }
+            if (symptoms.equals("discomfort")) {
+                discomfort = true;
+            }
+            if (symptoms.equals("nausea")) {
+                nausea = true;
+            }
+            if (symptoms.equals("pain")) {
+                pain = true;
+            }
+            if (symptoms.equals("drowsiness")) {
+                drowsiness = true;
+            }
+            if (symptoms.equals("lost appetite") || symptoms.equals("loss of appetite")) {
+                lost_appetite = true;
+            }
+            if (symptoms.equals("dehydration")) {
+                dehydration = true;
+            }
+        }
+
+        //Calling all methods
+        while (!isFound) {
+            malariaTest();
+            pregnancyTest();
+        }
+
+    }
+
+    public static void pregnancyTest() {
+        if (vomiting && fatigue && isFemale) {
+            System.out.println("You are pregnant!");
+        }
+        isFound = true;
+    }
+
+    public static void malariaTest() {
+        if (headache & fever) {
+            System.out.println("You are having malaria");
+        }
+        isFound = true;
+    }
+
+    public static void notFound() {
+        System.out.println("Oops! We can't find out what's wrong!");
+        System.out.println("We advise that you seek a medical professional advice as soon as possible.");
     }
 }
